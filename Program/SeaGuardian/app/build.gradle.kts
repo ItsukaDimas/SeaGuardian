ff5d39373ba389d185ba3cdd7f149625da4f998d
ItsukaDimas <89897048+ItsukaDimas@users.noreply.github.com>
2024-06-10 13:04:09 +0900

    master: update versi fragment

--- Program/SeaGuardian/app/build.gradle.kts
+++ Program/SeaGuardian/app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 }
 
 dependencies {
-
+    val fragment_version = "1.6.1"
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -42,4 +42,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.fragment:fragment:$fragment_version")
 }
