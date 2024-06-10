4cc9491bf20a5c66dd1e7dad014f990987bdae24
ItsukaDimas <89897048+ItsukaDimas@users.noreply.github.com>
2024-06-10 14:29:26 +0900

    master:Menambahkan ActivityMainBinding

--- Program/SeaGuardian/app/src/main/java/com/if22dx/seaguardian/MainActivity.kt
+++ Program/SeaGuardian/app/src/main/java/com/if22dx/seaguardian/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.if22dx.seaguardian
 
+import android.app.Activity
 import android.os.Bundle
+import android.view.inputmethod.InputBinding
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.if22dx.seaguardian.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +23,7 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
     }
 }
