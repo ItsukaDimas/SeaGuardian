54b8892517dfd1cf5cd5b167442357cf4c09c784
ItsukaDimas <89897048+ItsukaDimas@users.noreply.github.com>
2024-06-10 14:45:55 +0900

    master: import material TabLayout

--- Program/SeaGuardian/app/src/main/java/com/if22dx/seaguardian/MainActivity.kt
+++ Program/SeaGuardian/app/src/main/java/com/if22dx/seaguardian/MainActivity.kt
@@ -10,13 +10,15 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.if22dx.seaguardian.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    //private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
